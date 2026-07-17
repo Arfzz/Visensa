ba0752ee3a40a115b9bb3138fe2f03a6e77dd8b8
kenjidotkom <choirunok123@gmail.com>
2026-07-17 13:46:17 +0700

    feat: update routes for new doctor pages

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Camera from "./pages/camera/Camera";
 import SessionComplete from "./pages/session-complete/SessionComplete";
 
 import LoginDoctor from './pages/login/LoginDoctor';
+import RegisterDoctor from './pages/register/RegisterDoctor';
 import Dashboard from "./pages/admin-dashboard/Dashboard";
 
 import { useLenis } from "./hooks/useLenis";
@@ -76,6 +77,9 @@ function App() {
 
         {/* Jalur ke Halaman Login Dokter */}
         <Route path="/login-doctor" element={<LoginDoctor />} />
+
+        {/* Jalur ke Halaman Register Dokter */}
+        <Route path="/register-doctor" element={<RegisterDoctor />} />
 
         {/* Jalur ke Halaman Dashboard Admin */}
         <Route path="/admin-dashboard" element={<Dashboard />} />
